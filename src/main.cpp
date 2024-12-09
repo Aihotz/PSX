9201454be2ee64451233871f8699cd92197fce82
Aihotz Arruti <aihotz_arruti_aldama@outlook.es>
2024-12-10 00:41:09 +0100

    something rendering already!

--- src/main.cpp
+++ src/main.cpp
@@ -13,6 +13,8 @@
 
 #include <iostream>
 
+#include <chrono>
+
 void DebugCallback(
     gl::GLenum        source,
     gl::GLenum        type,
@@ -67,10 +69,14 @@ void DebugCallback(
     printf("%d: %s of %s severity, raised from %s: %s\n", id, type_str.c_str(), severity_str.c_str(), source_str.c_str(), message);
 }
 
-// Main code
+void Initialize();
+void Update(float delta);
+void Render();
+void Shutdown();
+
 int main(int, char**)
 {
-    // Setup SDL
+    // setup SDL
     if (!SDL_Init(SDL_INIT_VIDEO | SDL_INIT_GAMEPAD))
     {
         printf("Error: SDL_Init(): %s\n", SDL_GetError());
@@ -78,13 +84,13 @@ int main(int, char**)
     }
 
     // GL 3.0 + GLSL 130
-    const char* glsl_version = "#version 130";
+    const char* glsl_version = "#version 460";
     SDL_GL_SetAttribute(SDL_GL_CONTEXT_FLAGS, 0);
     SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
     SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
     SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 0);
 
-    // Create window with graphics context
+    // create window with graphics context
     SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
     SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, 24);
     SDL_GL_SetAttribute(SDL_GL_STENCIL_SIZE, 8);
@@ -107,7 +113,7 @@ int main(int, char**)
     SDL_GL_SetSwapInterval(1); // Enable vsync
     SDL_ShowWindow(window);
 
-    // Setup Dear ImGui context
+    // setup Dear ImGui context
     IMGUI_CHECKVERSION();
     ImGui::CreateContext();
     ImGuiIO& io = ImGui::GetIO();
@@ -115,10 +121,9 @@ int main(int, char**)
     io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard; // Enable Keyboard Controls
     io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;  // Enable Gamepad Controls
 
-    // Setup Dear ImGui style
     ImGui::StyleColorsDark();
 
-    // Setup Platform/Renderer backends
+    // setup Platform/Renderer backends
     ImGui_ImplSDL3_InitForOpenGL(window, gl_context);
     ImGui_ImplOpenGL3_Init(glsl_version);
 
@@ -129,31 +134,29 @@ int main(int, char**)
     gl::glEnable(gl::GL_DEBUG_OUTPUT);
     gl::glDebugMessageCallback(DebugCallback, 0);
 
-    ImVec4 clear_color = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
-
     if (SDL_GL_LoadLibrary(nullptr) == false)
     {
         printf("Error loading OpenGL library: %s\n", SDL_GetError());
         return -1;
     }
 
-    // Main loop
-    bool done = false;
-    while (!done)
+    Initialize();
+
+    // initialize delta time
+    // dont use auto keyword
+    std::chrono::high_resolution_clock::time_point last_time = std::chrono::high_resolution_clock::now();
+
+    bool running = true;
+    while (running)
     {
-        // Poll and handle events (inputs, window resize, etc.)
-        // You can read the io.WantCaptureMouse, io.WantCaptureKeyboard flags to tell if dear imgui wants to use your inputs.
-        // - When io.WantCaptureMouse is true, do not dispatch mouse input data to your main application, or clear/overwrite your copy of the mouse data.
-        // - When io.WantCaptureKeyboard is true, do not dispatch keyboard input data to your main application, or clear/overwrite your copy of the keyboard data.
-        // Generally you may always pass all inputs to dear imgui, and hide them from your application based on those two flags.
         SDL_Event event;
         while (SDL_PollEvent(&event))
         {
             ImGui_ImplSDL3_ProcessEvent(&event);
             if (event.type == SDL_EVENT_QUIT)
-                done = true;
+                running = false;
             if (event.type == SDL_EVENT_WINDOW_CLOSE_REQUESTED && event.window.windowID == SDL_GetWindowID(window))
-                done = true;
+                running = false;
         }
         if (SDL_GetWindowFlags(window) & SDL_WINDOW_MINIMIZED)
         {
@@ -161,23 +164,32 @@ int main(int, char**)
             continue;
         }
 
-        // Start the Dear ImGui frame
+        // compute delta time
+        std::chrono::high_resolution_clock::time_point current_time = std::chrono::high_resolution_clock::now();
+        float                                          delta        = std::chrono::duration<float>(current_time - last_time).count();
+        last_time                                                   = current_time;
+
+        Update(delta);
+
+        // start the Dear ImGui frame
         ImGui_ImplOpenGL3_NewFrame();
         ImGui_ImplSDL3_NewFrame();
         ImGui::NewFrame();
 
-        ImGui::ShowDemoWindow();
+        gl::glClear(gl::GL_COLOR_BUFFER_BIT);
 
-        // Rendering
+        Render();
+
+        // rendering
         ImGui::Render();
         gl::glViewport(0, 0, (int)io.DisplaySize.x, (int)io.DisplaySize.y);
-        gl::glClearColor(clear_color.x * clear_color.w, clear_color.y * clear_color.w, clear_color.z * clear_color.w, clear_color.w);
-        gl::glClear(gl::GL_COLOR_BUFFER_BIT);
         ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
         SDL_GL_SwapWindow(window);
     }
 
-    // Cleanup
+    Shutdown();
+
+    // cleanup
     ImGui_ImplOpenGL3_Shutdown();
     ImGui_ImplSDL3_Shutdown();
     ImGui::DestroyContext();
@@ -187,4 +199,357 @@ int main(int, char**)
     SDL_Quit();
 
     return 0;
+}
+
+#define GLM_FORCE_RADIANS
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+
+using namespace gl;
+
+struct Camera
+{
+    glm::mat4 m_viewMatrix;
+    glm::mat4 m_projectionMatrix;
+};
+
+struct Vertex
+{
+    enum AttributeLocation
+    {
+        POSITION            = 0,
+        NORMAL              = 1,
+        TEXTURE_COORDINATES = 2,
+        COLOR               = 3
+    };
+
+    glm::vec3 position;
+    glm::vec3 normal;
+    glm::vec2 textureCoordinates;
+    glm::vec3 color;
+};
+
+struct Object
+{
+    glm::vec3 m_position;
+    glm::vec3 m_rotation;
+    glm::vec3 m_scale;
+
+    GLuint m_vao;
+    GLuint m_vbo;
+
+    std::vector<Vertex> m_vertices;
+
+    GLuint m_texture;
+
+    void Initialize()
+    {
+        // initialize the object
+        // generate VAO and VBO
+        glGenVertexArrays(1, &m_vao);
+        glGenBuffers(1, &m_vbo);
+
+        glBindBuffer(GL_ARRAY_BUFFER, m_vbo);
+        glBindVertexArray(m_vao);
+
+        // upload mesh
+        glBufferData(GL_ARRAY_BUFFER, sizeof(Vertex) * m_vertices.size(), m_vertices.data(), GL_STATIC_DRAW);
+
+        // populate VBO
+        glEnableVertexAttribArray(Vertex::POSITION);
+        glEnableVertexAttribArray(Vertex::NORMAL);
+        glEnableVertexAttribArray(Vertex::TEXTURE_COORDINATES);
+        glEnableVertexAttribArray(Vertex::COLOR);
+
+        glVertexAttribPointer(Vertex::POSITION, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), reinterpret_cast<void*>(offsetof(Vertex, position)));
+        glVertexAttribPointer(Vertex::NORMAL, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), reinterpret_cast<void*>(offsetof(Vertex, normal)));
+        glVertexAttribPointer(Vertex::TEXTURE_COORDINATES, 2, GL_FLOAT, GL_FALSE, sizeof(Vertex), reinterpret_cast<void*>(offsetof(Vertex, textureCoordinates)));
+        glVertexAttribPointer(Vertex::COLOR, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), reinterpret_cast<void*>(offsetof(Vertex, color)));
+
+        glBindBuffer(GL_ARRAY_BUFFER, 0);
+        glBindVertexArray(0);
+
+        // initialize texture
+        glGenTextures(1, &m_texture);
+        glBindTexture(GL_TEXTURE_2D, m_texture);
+
+        // set texture parameters
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+
+        // send texture data
+        // create a 16x16 black and white checkerboard pattern
+        uint8_t data[16 * 16 * 4];
+        for (int i = 0; i < 16; i++)
+        {
+            for (int j = 0; j < 16; j++)
+            {
+                uint8_t color              = (i + j) % 2 == 0 ? 255 : 0;
+                data[(i * 16 + j) * 4 + 0] = color;
+                data[(i * 16 + j) * 4 + 1] = color;
+                data[(i * 16 + j) * 4 + 2] = color;
+                data[(i * 16 + j) * 4 + 3] = 255;
+            }
+        }
+
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, 16, 16, 0, GL_RGBA, GL_UNSIGNED_BYTE, data);
+
+        // unbind texture
+        glBindTexture(GL_TEXTURE_2D, 0);
+    };
+
+    void Render(const glm::mat4& view_projection, GLuint shader_program) const
+    {
+        // render the object
+        glBindVertexArray(m_vao);
+
+        glUseProgram(shader_program);
+
+        glm::mat4 model =
+            glm::translate(glm::mat4(1.0f), m_position) *
+            glm::rotate(glm::mat4(1.0f), m_rotation.x, glm::vec3(1, 0, 0)) *
+            glm::rotate(glm::mat4(1.0f), m_rotation.y, glm::vec3(0, 1, 0)) *
+            glm::rotate(glm::mat4(1.0f), m_rotation.z, glm::vec3(0, 0, 1)) *
+            glm::scale(glm::mat4(1.0f), m_scale);
+
+        // send uniforms to shader: viewproj, model and texture data
+        glUniformMatrix4fv(0, 1, GL_FALSE, &view_projection[0][0]);
+        glUniformMatrix4fv(1, 1, GL_FALSE, &model[0][0]);
+
+        // send texture
+        glActiveTexture(GL_TEXTURE0);
+        glBindTexture(GL_TEXTURE_2D, m_texture);
+
+        glDrawArrays(GL_TRIANGLES, 0, m_vertices.size());
+
+        glBindVertexArray(0);
+    };
+
+    void Shutdown()
+    {
+        // shutdown the object
+        glDeleteVertexArrays(1, &m_vao);
+        glDeleteBuffers(1, &m_vbo);
+    };
+};
+
+namespace
+{
+    Camera              camera;
+    std::vector<Object> objects;
+
+    GLuint shader;
+} // namespace
+
+void Initialize()
+{
+    // vsync
+    SDL_GL_SetSwapInterval(1);
+
+    // enable backface removal
+    glEnable(GL_CULL_FACE);
+    glCullFace(GL_BACK);
+    glFrontFace(GL_CCW);
+
+    // enable depth buffer
+    glEnable(GL_DEPTH_TEST);
+    glDepthFunc(GL_LESS);
+    glDepthRange(0, 1);
+
+    // enable scissor test
+    glEnable(GL_SCISSOR_TEST);
+
+    // polygon mode: fill the triangles by default
+    glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
+
+    // no colors or normals needed for the cube, only the positions and uvs
+    static const std::vector<Vertex> cube_primitive {
+        // Front face
+        // First triangle
+        { { -0.5f, -0.5f, 0.5f }, {}, { 0.0f, 0.0f }, {} },
+        { { 0.5f, -0.5f, 0.5f }, {}, { 1.0f, 0.0f }, {} },
+        { { 0.5f, 0.5f, 0.5f }, {}, { 1.0f, 1.0f }, {} },
+        // Second triangle
+        { { 0.5f, 0.5f, 0.5f }, {}, { 1.0f, 1.0f }, {} },
+        { { -0.5f, 0.5f, 0.5f }, {}, { 0.0f, 1.0f }, {} },
+        { { -0.5f, -0.5f, 0.5f }, {}, { 0.0f, 0.0f }, {} },
+
+        // Back face
+        // First triangle
+        { { 0.5f, -0.5f, -0.5f }, {}, { 0.0f, 0.0f }, {} },
+        { { -0.5f, -0.5f, -0.5f }, {}, { 1.0f, 0.0f }, {} },
+        { { -0.5f, 0.5f, -0.5f }, {}, { 1.0f, 1.0f }, {} },
+        // Second triangle
+        { { -0.5f, 0.5f, -0.5f }, {}, { 1.0f, 1.0f }, {} },
+        { { 0.5f, 0.5f, -0.5f }, {}, { 0.0f, 1.0f }, {} },
+        { { 0.5f, -0.5f, -0.5f }, {}, { 0.0f, 0.0f }, {} },
+
+        // Left face
+        // First triangle
+        { { -0.5f, -0.5f, -0.5f }, {}, { 0.0f, 0.0f }, {} },
+        { { -0.5f, -0.5f, 0.5f }, {}, { 1.0f, 0.0f }, {} },
+        { { -0.5f, 0.5f, 0.5f }, {}, { 1.0f, 1.0f }, {} },
+        // Second triangle
+        { { -0.5f, 0.5f, 0.5f }, {}, { 1.0f, 1.0f }, {} },
+        { { -0.5f, 0.5f, -0.5f }, {}, { 0.0f, 1.0f }, {} },
+        { { -0.5f, -0.5f, -0.5f }, {}, { 0.0f, 0.0f }, {} },
+
+        // Right face
+        // First triangle
+        { { 0.5f, -0.5f, 0.5f }, {}, { 0.0f, 0.0f }, {} },
+        { { 0.5f, -0.5f, -0.5f }, {}, { 1.0f, 0.0f }, {} },
+        { { 0.5f, 0.5f, -0.5f }, {}, { 1.0f, 1.0f }, {} },
+        // Second triangle
+        { { 0.5f, 0.5f, -0.5f }, {}, { 1.0f, 1.0f }, {} },
+        { { 0.5f, 0.5f, 0.5f }, {}, { 0.0f, 1.0f }, {} },
+        { { 0.5f, -0.5f, 0.5f }, {}, { 0.0f, 0.0f }, {} },
+
+        // Top face
+        // First triangle
+        { { -0.5f, 0.5f, 0.5f }, {}, { 0.0f, 0.0f }, {} },
+        { { 0.5f, 0.5f, 0.5f }, {}, { 1.0f, 0.0f }, {} },
+        { { 0.5f, 0.5f, -0.5f }, {}, { 1.0f, 1.0f }, {} },
+        // Second triangle
+        { { 0.5f, 0.5f, -0.5f }, {}, { 1.0f, 1.0f }, {} },
+        { { -0.5f, 0.5f, -0.5f }, {}, { 0.0f, 1.0f }, {} },
+        { { -0.5f, 0.5f, 0.5f }, {}, { 0.0f, 0.0f }, {} },
+
+        // Bottom face
+        // First triangle
+        { { -0.5f, -0.5f, -0.5f }, {}, { 0.0f, 0.0f }, {} },
+        { { 0.5f, -0.5f, -0.5f }, {}, { 1.0f, 0.0f }, {} },
+        { { 0.5f, -0.5f, 0.5f }, {}, { 1.0f, 1.0f }, {} },
+        // Second triangle
+        { { 0.5f, -0.5f, 0.5f }, {}, { 1.0f, 1.0f }, {} },
+        { { -0.5f, -0.5f, 0.5f }, {}, { 0.0f, 1.0f }, {} },
+        { { -0.5f, -0.5f, -0.5f }, {}, { 0.0f, 0.0f }, {} }
+    };
+
+    objects.push_back(
+        Object {
+            .m_position { 0.f, 0.f, 0.f },
+            .m_rotation { 0.f, 0.f, 0.f },
+            .m_scale { 1.0f, 1.0f, 1.0f },
+
+            .m_vertices = cube_primitive,
+        });
+
+    camera.m_viewMatrix       = glm::lookAt(glm::vec3(0, 0, 3), glm::vec3(0, 0, 0), glm::vec3(0, 1, 0));
+    camera.m_projectionMatrix = glm::perspective(glm::radians(45.0f), 1280.0f / 720.0f, 0.1f, 100.0f);
+
+    for (Object& object : objects)
+        object.Initialize();
+
+    // create shader
+    const char* vertex_shader_source = R"(
+		#version 460 core
+
+        layout(location = 0) in vec3 position;
+		layout(location = 2) in vec2 textureCoordinates;
+
+		out vec2 fragTextureCoordinates;
+
+		layout(location = 0) uniform mat4 viewproj;
+		layout(location = 1) uniform mat4 model;
+
+		void main()
+		{
+			gl_Position = viewproj * model * vec4(position, 1.0f);
+			fragTextureCoordinates = textureCoordinates;
+		}
+	)";
+
+    const char* fragment_shader_source = R"(
+		#version 460 core
+
+		in vec2 fragTextureCoordinates;
+
+		out vec4 color;
+
+		layout(binding = 0) uniform sampler2D textureData;
+
+		void main()
+		{
+			color = texture(textureData, fragTextureCoordinates);
+		}
+	)";
+
+    GLuint vertex_shader = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vertex_shader, 1, &vertex_shader_source, nullptr);
+    glCompileShader(vertex_shader);
+
+    // print shader compilation errors
+    GLint success;
+    glGetShaderiv(vertex_shader, GL_COMPILE_STATUS, &success);
+    if (!success)
+    {
+        GLchar infoLog[512];
+        glGetShaderInfoLog(vertex_shader, 512, nullptr, infoLog);
+        std::cerr << "Error: Shader compilation failed: " << infoLog << std::endl;
+    }
+
+    GLuint fragment_shader = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragment_shader, 1, &fragment_shader_source, nullptr);
+    glCompileShader(fragment_shader);
+
+    // print shader compilation errors
+    glGetShaderiv(fragment_shader, GL_COMPILE_STATUS, &success);
+    if (!success)
+    {
+        GLchar infoLog[512];
+        glGetShaderInfoLog(fragment_shader, 512, nullptr, infoLog);
+        std::cerr << "Error: Shader compilation failed: " << infoLog << std::endl;
+    }
+
+    shader = glCreateProgram();
+    glAttachShader(shader, vertex_shader);
+    glAttachShader(shader, fragment_shader);
+    glLinkProgram(shader);
+
+    // print linking errors
+    glGetProgramiv(shader, GL_LINK_STATUS, &success);
+    if (!success)
+    {
+        GLchar infoLog[512];
+        glGetProgramInfoLog(shader, 512, nullptr, infoLog);
+        std::cerr << "Error: Shader linking failed: " << infoLog << std::endl;
+    }
+
+    glDeleteShader(vertex_shader);
+    glDeleteShader(fragment_shader);
+}
+
+void Update(float delta)
+{
+    // rotate all objects around the vertical axis at a rate of 360 degrees per 5 seconds
+    for (Object& object : objects)
+    {
+        object.m_rotation.y += glm::radians(360.0f / 5.0f) * delta;
+
+        // also rotate slightly around the right axis, but slower
+        object.m_rotation.x += glm::radians(360.0f / 20.0f) * delta;
+    }
+}
+
+void Render()
+{
+    static constexpr glm::ivec2 window_size = glm::ivec2 { 1280, 720 };
+
+    glViewport(0, 0, window_size.x, window_size.y);
+    glScissor(0, 0, window_size.x, window_size.y);
+    glClearColor(0.45f, 0.55f, 0.60f, 1.00f);
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+    glm::mat4 view_projection = camera.m_projectionMatrix * camera.m_viewMatrix;
+
+    for (const Object& object : objects)
+        object.Render(view_projection, shader);
+}
+
+void Shutdown()
+{
+    for (Object& object : objects)
+        object.Shutdown();
 }
